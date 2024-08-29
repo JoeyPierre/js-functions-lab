05fcab9c4514ea6eef54f4fbea06368a01ab0f7c
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-08-29 09:35:17 -0400

    forth save

--- app.js
+++ app.js
@@ -45,3 +45,42 @@ function isCharAVowel(letter) {
 }
 
 console.log("Exercise 3 Result:", isCharAVowel("a"));
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+function generateEmail(name, domain) {
+  return name + "@" + domain;
+}
+
+console.log("Exercise 4 Result:", generateEmail("johnsmith", "example.com"));
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+function greetUser(name, timeOfDay) {
+  if (timeOfDay === "morning") {
+    return `Good Morning, ${name}`;
+  } else if (timeOfDay === "afternoon") {
+    return `Good Morning, ${name}`;
+  } else if (timeOfDay === "evening") {
+    return `Good Evening, ${name}`;
+  } else {
+    return `Hello ${name}`;
+  }
+}
+console.log("Exercise 5 Result:", greetUser("Sam", "morning"));
